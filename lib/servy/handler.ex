e0291e62544e7a79cfdf27cf11a2c843058cbcb0
Julio Linarez <Julio.linarez@dualbootpartners.com>
2023-11-05 08:22:45 -0300

    User fetcher to manage async tasks

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,9 +3,15 @@ defmodule Servy.Handler do
   import Servy.File, only: [handle_file: 2, handle_extension: 1]
   import Servy.Parser, only: [parse: 1]
 
+  alias Mix.Dep.Fetcher
+  alias Mix.Dep.Fetcher
+  alias Mix.Dep.Fetcher
+  alias Mix.Dep.Fetcher
+  alias Mix.Dep.Fetcher
   alias Servy.Conv
   alias Servy.BearController
-
+  alias Servy.VideoCam
+  alias Servy.Fetcher
 
   @moduledoc """
     Handles HTTP requests.
@@ -25,13 +31,31 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{method: "GET", path: "/hibernate/" <> time } = conv) do
-    time |> String.to_integer |> :timer.sleep
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
+    parent = self()
+
+    pid4 = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
+    snapshots =
+      ["cam1", "cam2", "cam3"]
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
+
+
+
+    where_is_bigfoot = Task.await(pid4)
+
+    %{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
+
+  end
+
+  def route(%Conv{method: "GET", path: "/hibernate/" <> time} = conv) do
+    time |> String.to_integer() |> :timer.sleep()
 
     %{conv | status: 200, resp_body: "Awake!"}
   end
 
-  def route(%Conv{method: "GET", path: "/kaboom/"}) do
+  def route(%Conv{method: "GET", path: "/kaboom"}) do
     raise "kaboom!"
   end
 
