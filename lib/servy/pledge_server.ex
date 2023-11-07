0e929fc3226b325aec09623adf79afc418a8fe83
Julio Linarez <Julio.linarez@dualbootpartners.com>
2023-11-07 02:16:14 -0300

    usando pledge serve del video tla cual.

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,67 +1,79 @@
 defmodule Servy.PledgeServer do
 
-  @name __MODULE__
+  @name :pledge_server
+
+  # Client Interface
 
   def start do
-    IO.puts("Starting the pledge server...")
+    IO.puts "Starting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
     Process.register(pid, @name)
     pid
   end
 
-  def listen_loop(state) do
-    IO.puts("\n Waiting for a message ...")
-
-    receive do
-      {sender, :create, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} pledged #{amount}")
-        IO.puts("New state is #{inspect new_state}")
-        send sender, {:response, id}
-        listen_loop(new_state)
-      {sender, :recent} ->
-        send sender, {:response, state}
-        IO.puts("Sent pledges to #{inspect state}")
-        listen_loop(state)
-      end
-  end
-
   def create_pledge(name, amount) do
-    send @name, {self(), :create, name, amount}
-    receive do
-      {:response, status} ->
-        status
-    end
+    send @name, {self(), :create_pledge, name, amount}
+
+    receive do {:response, status} -> status end
   end
 
   def recent_pledges do
-    send @name, {self(), :recent}
+    send @name, {self(), :recent_pledges}
+
+    receive do {:response, pledges} -> pledges end
+  end
+
+  def total_pledged do
+    send @name, {self(), :total_pledged}
+
+    receive do {:response, total} -> total end
+  end
+
+  # Server
+
+  def listen_loop(state) do
     receive do
-      {:response, pledges} ->
-       pledges
+      {sender, :create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [ {name, amount} | most_recent_pledges ]
+        send sender, {:response, id}
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        listen_loop(state)
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+        send sender, {:response, total}
+        listen_loop(state)
+      unexpected ->
+        IO.puts "Unexpected messaged: #{inspect unexpected}"
+        listen_loop(state)
     end
 
   end
 
-
-  def run do
-    pid = spawn(Servy.PledgeServer, :listen_loop, [[]])
-
-    send pid, {self(), :create, "larry", 10}
-    send pid, {self(), :create, "moe", 20}
-    send pid, {self(), :create, "curly", 30}
-    send pid, {self(), :create, "daisy", 40}
-    send pid, {self(), :create, "grace", 50}
-
-    send pid, {self(), :recent}
-
-    receive do {:response, pledges} -> IO.inspect pledges end
-    pid
-  end
-
-
   defp send_pledge_to_service(_name, _amount) do
+    # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
+
 end
+
+alias Servy.PledgeServer
+
+pid = PledgeServer.start()
+
+send pid, {:stop, "hammertime"}
+
+IO.inspect PledgeServer.create_pledge("larry", 10)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("curly", 30)
+IO.inspect PledgeServer.create_pledge("daisy", 40)
+IO.inspect PledgeServer.create_pledge("grace", 50)
+
+IO.inspect PledgeServer.recent_pledges()
+
+IO.inspect PledgeServer.total_pledged()
+
+IO.inspect Process.info(pid, :messages)
