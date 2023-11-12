5ae43166099a303aa839ee52eb5f690137b23ac0
Julio Linarez <Julio.linarez@dualbootpartners.com>
2023-11-11 21:02:47 -0300

    Add linked process to kikc_starter

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -6,10 +6,20 @@ defmodule Servy.KickStarter do
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call __MODULE__, :get_server
+  end
+
+  # Server callbacks
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_http_server()
     {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
@@ -22,7 +32,7 @@ defmodule Servy.KickStarter do
     IO.puts "Starting the HTTP server..."
     server_pid = spawn_link(Servy.HttpServer, :start, [4000])
     # Process.link(server_pid)
-    Process.register(server_pid, :http_server)
+    # Process.register(server_pid, :http_server)
     server_pid
   end
 
